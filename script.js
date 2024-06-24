a5fabde4d046ff7af6d29bd2a73b9ec7e24142c9
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-24 21:35:35 +0200

    [BONUS] Ho stilizzato la pagina e l'ho resa un po' più interessante studiacchiando in autonomia js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  PASSWORD GENERATOR
  *****************************/
 
-
+function toggleGen() {
 let name = prompt ('Inserisci qui il tuo nome')
 
 let surname = prompt ('Inserisci qui il tuo cognome')
@@ -10,4 +10,6 @@ let surname = prompt ('Inserisci qui il tuo cognome')
 let fav_color = prompt ('Inserisci qui il tuo colore preferito')
 
 
-document.getElementById("generator").innerHTML = name + surname + fav_color + '23'
+document.getElementById("target").innerHTML = name + surname + fav_color + '23'
+
+}
