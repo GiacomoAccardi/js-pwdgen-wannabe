985b98c34d0c4ae275c0dfb30178caef62a8edab
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-24 15:29:19 +0200

    [UPD] Esercizio completato

---
+++ script.js
@@ -0,0 +1,13 @@
+/**************************************
+ PASSWORD GENERATOR
+ *****************************/
+
+
+let name = prompt ('Inserisci qui il tuo nome')
+
+let surname = prompt ('Inserisci qui il tuo cognome')
+
+let fav_color = prompt ('Inserisci qui il tuo colore preferito')
+
+
+document.getElementById("generator").innerHTML = name + surname + fav_color + '23'
